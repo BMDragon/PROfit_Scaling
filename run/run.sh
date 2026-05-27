a5260ce19b9cf11ef643cb37276a69a60103c8e3
bmweiss <bmweiss10@gmail.com>
2026-05-27 10:09:00 -0400

    Add conditional to bash script

--- run/run.sh
+++ run/run.sh
@@ -4,8 +4,10 @@ for i in {50..500..25}
 do
 (
     python generateXML.py $i 10
-    (time PROfit -x .././xml/scale_${i}bins_10pulls.xml -t scaling${i}bins_10pulls process) > .././time_logs/log_${i}bins_10pulls_time.txt 2>&1
-    grep user .././time_logs/log_${i}bins_10pulls_time.txt > .././time_logs/user_${i}bins_10pulls_time.txt
+    if [ ! -f "./scaling${i}bins_10pulls process" ]; then
+        (time PROfit -x .././xml/scale_${i}bins_10pulls.xml -t scaling${i}bins_10pulls process) > .././time_logs/log_${i}bins_10pulls_time.txt 2>&1
+        grep user .././time_logs/log_${i}bins_10pulls_time.txt > .././time_logs/user_${i}bins_10pulls_time.txt
+    fi
     PROfit -x .././xml/scale_${i}bins_10pulls.xml --tag scaling${i}bins_10pulls -v 1 -w 3 --log .././process_logs/log_${i}bins_10pulls.txt --progress --seed 314 --scale-by-width scale-test
     grep SCALE .././process_logs/log_${i}bins_10pulls.txt > .././scaling_outputs/scale_${i}bins_10pulls.txt
     echo "Finished ${i} bins, 10 pulls"
